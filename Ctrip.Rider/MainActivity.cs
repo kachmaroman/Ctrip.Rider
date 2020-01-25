3fdb1d7f2a80cdb21f967e43019675f73c905646
Roman Kachmar <lnt397@gmail.com>
2020-01-25 15:57:34 +0200

    Initial

---
+++ Ctrip.Rider/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Firebase.Database;
+using Firebase;
+
+namespace Ctrip.Rider
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        FirebaseDatabase database;
+
+        Button btnTestConnection;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btnTestConnection = FindViewById<Button>(Resource.Id.myButton);
+            btnTestConnection.Click += BtnTestConnection_Click;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void BtnTestConnection_Click(object sender, EventArgs e)
+        {
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetApplicationId("ctrip-50eab")
+                    .SetApiKey("AIzaSyDBk-f9zqpg1uGZYAHUt5kV8xbOxGQiS9w")
+                    .SetDatabaseUrl("https://ctrip-50eab.firebaseio.com")
+                    .SetStorageBucket("ctrip-50eab.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            DatabaseReference databaseReference = database.GetReference("UserSupport");
+            databaseReference.SetValue("Ticket1");
+
+            Toast.MakeText(this, "Completed", ToastLength.Short).Show();
+        }
+    }
+}
